9fa6e0fa82d13e34c95c50da75189de3f376385b
komba <komba@LAPTOP-AIANJH69>
2021-09-30 09:11:27 +0500

    Добавьте файлы проекта.

---
+++ pz5/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace pz5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int k;
+            for ( k = 0; k <= 80; k += 4)
+            {
+                Console.WriteLine(k);
+            }
+        }
+    }
+}

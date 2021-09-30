f3ce363d28b44568b5311c4e550b1367c7ffd604
Егор Гривко <kombad56@mail.ru>
2021-09-30 11:50:47 +0500

    test edit

--- pz5/Program.cs
+++ pz5/Program.cs
@@ -6,11 +6,50 @@ namespace pz5
     {
         static void Main(string[] args)
         {
+            int i, n, r, y, j ,m;
+            char smv = 'c';
+            //// 1 задание
+            Console.WriteLine("\nЗадние 1:");
             int k;
-            for ( k = 0; k <= 80; k += 4)
+            for (k = 0; k <= 80; k += 4)
             {
                 Console.WriteLine(k);
             }
-        }
-    }
+            ///2 задание Вводим  число 3 и полчим ответ
+            Console.WriteLine("\nЗадние 2:");
+            n = 0;
+            while (n<=5)
+            {
+                n += 1;
+                Console.Write(smv + " ");
+                smv++;
+
+            }
+            Console.WriteLine("\nЗадние 3:");
+            /// 3 задание 
+            for (m = 0; m <= 4; m++)
+            {
+                for (int o = 0; o <= 2; o++) Console.Write("##");
+                Console.WriteLine("");
+            }
+            /// 4 задание
+              Console.WriteLine("\nЗадние 4:");
+            for (i = 0; i <= 100; i++)
+            {
+                if (i == 0) continue;
+                if ((i % 5) != 0) continue;
+                Console.WriteLine(i + " ");
+            }
+            Console.WriteLine("\nЗадние 5:");
+            /// 5 задание
+            i = 1;
+            j = 40;
+            for (j = 1, j = 40; j - i > 14; i++, j--) 
+            {
+                Console.WriteLine("i и y равны :" + i + " и " + j);
+            }
+
+        }      
+    }      
+                
 }

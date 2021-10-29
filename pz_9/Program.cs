09c4f32655007680fb879cb1c73ae35d0c401b74
Егор Гривко <kombad56@mail.ru>
2021-10-29 13:19:34 +0500

    test

---
+++ pz_9/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace pz_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] array = { 2, 121, 19, 4, -1, 1, 22, 32, -24, 62 };// массив
+            double max = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (Math.Abs(array[i]) > Math.Abs(max))
+                    max = array[i];
+            }
+            Console.WriteLine("max= {0}", max);
+            int x = 0, y = 0; // x=строчка, y=номер в строчке
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] > 0)
+                {
+                    x = i;
+                    for (int j = x + 1; j < array.Length; j++)
+                    {
+                        if (array[j] > 0)
+                        {
+                            y = j;
+                            break;
+                        }
+                    }
+                    break;
+                }
+            }
+            Console.WriteLine($"x={ x}\ny={ y} ");// вывод значения строки и номера
+            for (int i = 0; i < array.Length; i++)
+            {
+                
+                if (i == 5)
+                {
+                    Console.WriteLine();
+                }
+                Console.Write(array[i] + "\t");// написал в 2 строки
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}

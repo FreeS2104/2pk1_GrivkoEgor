c8cad0f96fbceff7453d33a529420109bbd7fd05
Егор Гривко <kombad56@mail.ru>
2021-11-08 18:44:26 +0500

    test edit

---
+++ pz_6/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace pz_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 2;
+            int n = int.Parse(Console.ReadLine());
+            while (n % i != 0)
+                i += 1;
+            Console.WriteLine(i);
+            ////
+        }
+    }
+}

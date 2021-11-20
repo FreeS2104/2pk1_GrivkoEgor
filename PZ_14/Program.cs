e2dabd1df8906e98c429b9f0cdab8aaf2af11b6d
Егор Гривко <kombad56@list.ru>
2021-11-20 20:56:07 +0500

    test edit

---
+++ PZ_14/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+
+namespace PZ_14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string pathToInput = @"D:\Egor\input.txt";
+            string pathToOutput = @"D:\Egor\output.txt";
+            int[] nums = new int[100];
+            File.Delete(pathToInput);
+            using (StreamWriter sw = File.AppendText(pathToInput))
+            {
+                Random rand = new Random();
+                for (int i = 0; i < 100; i++)
+                {
+                    nums[i] = rand.Next(0, 101);
+                    sw.WriteLine(nums[i]);
+                }
+            }
+            Array.Sort(nums);
+
+            using (StreamWriter sw = File.AppendText(pathToOutput))
+            {
+                for (int i = 0; i < nums.Length; i++)
+                    sw.WriteLine(nums[i]);
+            }
+        }
+    }
+}

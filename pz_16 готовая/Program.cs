46f531a993c68f558161e010654f7375026215d8
Егор Гривко <kombad56@list.ru>
2021-11-25 06:30:58 +0300

    test edit

---
+++ pz_16 готовая/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace pz_16_готовая
+{
+    class Program
+    {
+        static void GetLimetSumm(int n, out int ss)
+        {
+            ss = 0;
+            for (int i = 0; i <= n; i++)
+            {
+                ss += i;
+            }
+            Console.WriteLine(ss);
+        }
+        static void Main(string[] args)
+        {
+            int sum;
+            Console.WriteLine("Ведите число до которого хотите посчитать сумму :");
+            int n = Convert.ToInt32(Console.ReadLine());
+            GetLimetSumm(n, out sum);
+        }
+    }
+}

7597a0d4ba707e4c70b10af970c0089009a6fd22
Егор Гривко <kombad56@mail.ru>
2022-02-15 18:27:51 +0500

    pz_3 2 semestr

---
+++ pz_3/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace pz_3
+{
+    class Compony
+    {
+        public string name;
+        public int okpo;
+        public DateTime dateOFRegister;
+
+        public Compony(string name, int okpo, DateTime data)
+        {
+            this.name = name;
+            this.okpo = okpo;
+            this.dateOFRegister = data;
+           
+
+
+        }
+        public void GetCompanyInfoo()
+        {
+            Console.WriteLine($"Организация {name}, под номером OKPO {okpo} была создана {dateOFRegister}");
+        }
+    }
+
+}
